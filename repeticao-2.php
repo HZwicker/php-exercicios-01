f1a1bbcb20550b7c498b0f2fb5a6858c8c5611eb
hzwicker <hickzwicker@gmail.com>
2019-05-23 12:34:51 -0300

    final

---
+++ repeticao-2.php
@@ -0,0 +1,16 @@
+<?php
+
+/*$contador = $contador + 1 ;
+$contador += $contador;
+$contador++;*/
+
+
+
+
+for ($contador = 1 ; $contador <= 15 ;  $contador ++){
+    if ($contador == 13){
+        break;
+        //continue
+    }
+    echo "#$contador" . PHP_EOL;
+}
